a580c79122d9dc65c15d627f7815edc36ce10d8d
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-28 19:41:37 -0800

    Assign position before looping

--- src/dancer.js
+++ src/dancer.js
@@ -5,11 +5,11 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
 
-  this.step();
-
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
+
+  this.step();
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
